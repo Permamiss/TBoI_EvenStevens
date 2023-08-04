7697039dda4e36334f8686cad3c727777b28a39b
Permamiss <39325797+Permamiss@users.noreply.github.com>
2023-08-04 00:15:42 -0700

    Use optional arg in ModCallbacks.MC_POST_PICKUP_INIT, remove extraneous code from prior version

--- EvenStevens/main.lua
+++ EvenStevens/main.lua
@@ -13,17 +13,15 @@ end
 
 ---@param pickup EntityPickup
 function EvenStevens:OnPickupSpawned(pickup)
-	if pickup.Type == EntityType.ENTITY_PICKUP and pickup.Variant == PickupVariant.PICKUP_COLLECTIBLE then
+	if pickup.Type == EntityType.ENTITY_PICKUP then
 		-- if pickup ID is Steven and last killed variant was Steven Baby, then
 		if pickup.SubType == CollectibleType.COLLECTIBLE_STEVEN and lastKilledVariant == NPC_VARIANT_STEVEN_BABY then
 			lastKilledVariant = 0
-			local pos = pickup.Position
 			-- swap out original drop for Little Steven
 			pickup:Morph(pickup.Type, pickup.Variant, CollectibleType.COLLECTIBLE_LITTLE_STEVEN)
 		-- if pickup ID is Little Steven and last killed variant was Steven, then
 		elseif pickup.SubType == CollectibleType.COLLECTIBLE_LITTLE_STEVEN and lastKilledVariant == NPC_VARIANT_STEVEN then
 			lastKilledVariant = 0
-			local pos = pickup.Position
 			-- swap out original drop for Steven
 			pickup:Morph(pickup.Type, pickup.Variant, CollectibleType.COLLECTIBLE_STEVEN)
 		end
@@ -31,4 +29,4 @@ function EvenStevens:OnPickupSpawned(pickup)
 end
 
 EvenStevens:AddCallback(ModCallbacks.MC_POST_NPC_DEATH, EvenStevens.OnNPCDeath)
-EvenStevens:AddCallback(ModCallbacks.MC_POST_PICKUP_INIT, EvenStevens.OnPickupSpawned)
+EvenStevens:AddCallback(ModCallbacks.MC_POST_PICKUP_INIT, EvenStevens.OnPickupSpawned, PickupVariant.PICKUP_COLLECTIBLE)
